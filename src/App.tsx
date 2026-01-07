72725f0ed202d410b5fa4e791f129f0434df7870
Hiretek Solutions <hireteksolutions@gmail.com>
2026-01-07 10:29:14 +0530

    Fix routing and add home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./lib/auth";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Add from "./pages/add";
-// import Fbpost from "./pages/fbpost";
-// import Fbpost2 from "./pages/fbpost2";
-// import Ad from "./pages/Ad";
-// import Carouselfb from "./pages/carouselfb";
+import Home from "./pages/home";
 
 const queryClient = new QueryClient();
 
@@ -21,15 +16,14 @@ const App = () => (
       <BrowserRouter>
         <AuthProvider>
           <Routes>
-            {/* <Route path="/" element={<Navigate to="/add" replace />} /> */}
-            <Route path="/add" element={<ProtectedRoute><Add /></ProtectedRoute>} />
-            <Route path="/ad" element={<Add />} />
-            {/* <Route path="/fbpost" element={<Fbpost />} />
-            <Route path="/fbpost2" element={<Fbpost2 />} />
-            <Route path="/carouselfb" element={<Carouselfb />} /> */}
-            
-            {/* <Route path="*" element={<Navigate to="/ad" replace />} /> */}
+            {/* Redirect root to home */}
+            <Route path="/" element={<Navigate to="/home" replace />} />
 
+            {/* Home page */}
+            <Route path="/home" element={<Home />} />
+
+            {/* Fallback */}
+            <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
